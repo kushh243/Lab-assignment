dd35317aaa2651f7b9f6fdf594b78998a7dba4fb
kushh243 <202401210@daiict.ac.in>
2025-04-04 23:01:21 +0530

    Create top_k_frequent.cpp

---
+++ top_k_frequent.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        for (const auto& p : freq) {
+            minHeap.push({p.second, p.first});
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+
+        vector<int> result;
+        while (!minHeap.empty()) {
+            result.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n, k;
+    
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    
+    cout << "Enter the value of k: ";
+    cin >> k;
+
+    vector<int> result = sol.topKFrequent(nums, k);
+    
+    cout << "Top " << k << " frequent elements: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
